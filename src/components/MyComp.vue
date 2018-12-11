3d8fdd4194d4212628848116df0212854bc469a6
Unknown <rohan@actionindex.jp>
2018-12-11 16:26:32 +0900

    Added Panolens

---
+++ src/components/MyComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mycomp">
+    <h1>BOOM: {{ msg_text }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+
+
+@Component
+export default class MyComp extends Vue {
+  @Prop() private msg_text!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  padding-top: 10px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-content>
+      <!-- <HelloWorld/> -->
+    <MyComp msg_text="vue-bak" />
+    </v-content>
+      <Pano
+           type='video'
+            source="assets/video.mp4">
+        </Pano>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import MyComp from "@/components/MyComp.vue"
+import {Pano} from "vuejs-vr";
+
+@Component({
+  components: {
+    HelloWorld,
+    MyComp,
+    Pano
+  }
+})
+export default class Home extends Vue {}
+</script>
